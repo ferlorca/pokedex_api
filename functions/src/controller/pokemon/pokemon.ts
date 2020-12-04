4d38f57ce0e60bfbc55302262245d101462d4cf7
fernando lorca <fernando.g.lorca@gmail.com>
2020-12-04 11:57:01 +0100

    some changes

--- functions/src/controller/pokemon/pokemon.ts
+++ functions/src/controller/pokemon/pokemon.ts
@@ -28,47 +28,24 @@ export async function all(req: Request, res: Response) {
 async function getSnapshotReference(filter:Filter): Promise<FirebaseFirestore.QuerySnapshot<FirebaseFirestore.DocumentData>>{
     
     let query:FirebaseFirestore.Query = admin.firestore().collection(COLLECTION_POKEMON);
-    
+    const nameLang = filter.name ? filter.name?.language : "english";
+	query = query.orderBy(`name.${nameLang}`);
+	
     if(filter.type)
 		query = query.where("type", "array-contains-any", filter.type);
 	
 	if(filter.name && filter.name.text)
-		query = query.where(`name.${filter.name.language}`, '>=', filter.name.text);
-
-	const nameLang = filter.name ? filter.name?.language : "english";
-	query = query.orderBy(`name.${nameLang}`);
+		query = query.where(`name.${filter.name.language}`, '>=', filter.name.text);	
 
 	if(filter.amountPerPage !== -1){
 		const startAt =  filter.page !== 0 ? (filter.page * filter.amountPerPage ) : filter.page;
 		query = query.startAt(startAt).limit(filter.amountPerPage);
 	}	 
+
+	
     
     return query.get();    
 }
-
-// async function getQueryBaseObj(type:Between,str:string):Promise<any>{
-    
-//     const nuevao = await admin.firestore().collection(`${COLLECTION_POKEMON}`).doc("AuZXq3u7Cuzqkz6caaPE").collection(COLLECTION_BASE).get();  
-//     console.log(nuevao.docs[0].data());
-//     const nueva2o = await admin.firestore().collection(`${COLLECTION_POKEMON}`).doc("AuZXq3u7Cuzqkz6caaPE").get();  
-//     const lala = nueva2o.data();
-//     console.log(lala);
-
-
-//     let query:FirebaseFirestore.Query = await admin.firestore().collectionGroup(COLLECTION_BASE);  
-//     query = query.where(str, '>=', type.min).where(str, '<=', type.max);
-   
-//     const basesnapshot = await query.get();     
-
-//     const documentsData  = basesnapshot.docs.map(async (item:FirebaseFirestore.QueryDocumentSnapshot)=>{
-//         const aux = item.ref.parent.parent;
-//         const doc = aux ? await aux.get() :null;      
-//         const __id= doc?.id;
-//         return  doc ? {__id,...doc.data()} : null
-//     });
-//     const documentSnapshot = await Promise.all(documentsData);
-//     return documentSnapshot;
-// }
 
 
 export async function types(req: Request, res: Response) {
